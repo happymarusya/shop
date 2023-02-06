6e5890a953f004e840b024a9abee41c0a5216d52
happymarusya <imya_maria@mail.ru>
2023-02-06 14:27:06 +0100

    first commit

---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import { data } from './data';
+import { useState } from 'react';
+import Clothes from './Clothes';
+import Buttons from './Buttons';
+
+function Home() {
+    const [clothes, setClothes] = useState(data);
+
+    const chosenClothes = (searchTerm) => {
+    const newClothes = data.filter(element => element.searchTerm === searchTerm);
+        setClothes(newClothes);
+    }
+
+    return (
+        <div>
+            <div className='cont'>
+                <h2 className='back'>Free Standard Shipping</h2> 
+            </div>
+            <Buttons filteredClothes = {chosenClothes} setClothes = {setClothes} data = {data}/>
+            <Clothes anyClothes = {clothes}/>
+        </div>
+    );
+}
+export default Home;
